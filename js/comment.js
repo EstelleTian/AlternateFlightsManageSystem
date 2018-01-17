2730aa326f25db2a4fae0978e532c9014afca9a7
krorainaHub <zxy920411@163.com>
2018-01-17 13:33:36 +0800

    右键协调窗口列表节点库

---
+++ js/comment.js
@@ -0,0 +1,14 @@
+// 右键窗口
+var CollaborateDom = {
+    // 进港
+    ARR : '<div class="grid-table-collaborate-container"><ul class="collaborate-menu"><li><a href="javascript:;">预选备降<span class="glyphicon glyphicon-play"></span></a><ul><li><a href="javascript:;">吐鲁番</a></li><li><a href="javascript:;">克拉玛依</a></li><li><a href="javascript:;">伊宁</a></li><li><a href="javascript:;">喀什</a></li><li><a href="javascript:;">阿勒泰</a></li><li><a href="javascript:;">库车</a></li><li><a href="javascript:;">军民合用<span class="glyphicon glyphicon-play"></span></a><ul><li><a href="javascript:;">库尔勒</a></li><li><a href="javascript:;">和田</a></li><li><a href="javascript:;">哈密</a></li><li><a href="javascript:;">阿克苏</a></li></ul></li></ul></li><li><a href="javascript:;">确定备降<span class="glyphicon glyphicon-play"></span></a></li><li><a href="javascript:;">正班占用</a></li><li><a href="javascript:;">手工添计划</a></li></ul></div>',
+    // 出港
+    DEP : '',
+    // 疆内
+    AREA : '',
+    // 备降
+    PRE: '',
+    // 备降容量
+    CAPACITY : ''
+
+};

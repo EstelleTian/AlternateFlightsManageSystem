09f74b46373162b692d2dc2b2055da8913789bf5
krorainaHub <zxy920411@163.com>
2018-01-23 10:51:46 +0800

    完善模块html结构；设置默认活动模块;新增表单组件，实现进、出、备降、飞越模块初始化

--- js/layout-event.js
+++ js/layout-event.js
@@ -6,6 +6,21 @@
 
 
 var layoutEvent = function () {
+
+    /** 设置默认活动模块
+     *
+     *  确保第一个模块为活动模块
+     * */
+    var initDefaultActiveModule = function () {
+        $('.menu-bar li:first').addClass('active');
+        $('.main-area section:first').addClass('active');
+        // 下标大于0的移除active class
+        $('.menu-bar li:gt(0)').removeClass('active');
+        $('.main-area section:gt(0)').removeClass('active');
+
+    };
+
+
     /**
      *  菜单栏事件
      * */
@@ -32,10 +47,11 @@ var layoutEvent = function () {
         $('.logout button').on('click',function () {
             window.location.href = 'index.html';
         });
-    }
+    };
 
     return {
         init : function () {
+            initDefaultActiveModule();
             menu();
             logout();
         }

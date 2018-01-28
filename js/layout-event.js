3769ffc7913631b5f56e4f99bd66c4424e48a225
krorainaHub <zxy920411@163.com>
2018-01-28 17:04:36 +0800

    实现模块切换后，开启当前模块的定时查询数据与数据绘制,停止其他模块的定时请求数据和数据绘制

--- js/layout-event.js
+++ js/layout-event.js
@@ -21,27 +21,7 @@ var layoutEvent = function () {
     };
 
 
-    /**
-     *  菜单栏事件
-     * */
-    var menu = function () {
-        $('.menu-bar li').on('click',function () {
-            // 取得当前点击对象
-            var $that = $(this);
-            // 若当前点击对象已经是活动的对象()则不作操作
-            if($that.hasClass('active')){
-                return;
-            }
-            // 反之将当前点击对象及内容设置为活动对象
-            var index = $that.index();
-            $('.menu-bar li').removeClass('active');
-            $('.main-area section').removeClass('active');
-            $that.addClass('active');
-            $('.main-area section').eq(index).addClass('active');
-            //处理表格自适应TODO
-            $(window).trigger('resize')
-        });
-    };
+
     /**
      * 用户登出事件
      * */
@@ -54,7 +34,7 @@ var layoutEvent = function () {
     return {
         init : function () {
             initDefaultActiveModule();
-            menu();
+            // menu();
             // logout();
         }
     }

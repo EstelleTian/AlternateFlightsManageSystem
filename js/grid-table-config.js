c1081407af186ef41f3cded0e1f4965dc276080d
liutj@adcc.com.cn <liutj@adcc.com.cn>
2018-03-21 16:49:32 +0800

    备降计划整理右键协调显示逻辑

--- js/grid-table-config.js
+++ js/grid-table-config.js
@@ -521,7 +521,6 @@ var GridTableConfig = function () {
     };
     // 航班状态列数值格式化转换
     function formatterStatus(cellvalue, options, rowObject) {
-        debugger
         var code = app.statusCode;
         if($.isValidObject(code) && $.isValidVariable(code[cellvalue])){
            return code[cellvalue].text;
